6ef32c4760e0f16145a9f75560b50fe12f5fe400
Jonathan-Gaal <jonathangaal@pursuit.org>
2023-01-06 16:29:08 -0500

    built navBar.js and links are directing to home, index, and new

--- src/Components/Main.js
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-  return (
-    <div className="main">
-      <h1>Welcome To Captains Logs</h1>
-    </div>
-  );
-};
-
-export default Home;
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,16 +1,12 @@
 import { Link } from "react-router-dom";
-import "./NavBar.css";
 
 const NavBar = () => {
   return (
     <nav className="navBar">
-      <h1 className="welcomeh1">Welcome to Captain's Logs</h1>
-      <Link id="logs" to="/logs">
-        Logs
-      </Link>
-      <Link id="newLog" to="/logs/new">
-        New Log
-      </Link>
+      <h1>Budget App</h1>
+      <Link to="/">Home</Link>
+      <Link to="/transactions">Transactions</Link>
+      <Link to="/transactions/new">New</Link>
     </nav>
   );
 };
