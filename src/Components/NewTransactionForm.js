6f4b235994697e57ed4681fbf071eb82d4c48fc6
Jonathan-Gaal <jonathangaal@pursuit.org>
2023-01-10 17:27:56 -0500

    navigate?

--- src/Components/NewTransactionForm.js
+++ src/Components/NewTransactionForm.js
@@ -28,9 +28,12 @@ const NewTransactionForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post(`${API}/transactions`, newTransaction).catch((err) => {
-      console.error(err);
-    });
+    axios
+      .post(`${API}/transactions`, newTransaction)
+      .then(() => window.alert("hello"))
+      .catch((err) => {
+        console.error(err);
+      });
   };
 
   return (
